c4ec300a88737e0bb037c8b5dddc6aebff04f887
hosopy <dev.hosopy@gmail.com>
2016-08-24 15:37:44 +0900

    fix incorrect port description

--- server.rb
+++ server.rb
@@ -11,7 +11,7 @@ def main
   s = GRPC::RpcServer.new
   s.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
   s.handle(GreeterServer)
-  puts 'listening on addr=0.0.0.0:3000'
+  puts 'listening on addr=0.0.0.0:50051'
   s.run_till_terminated
 end
 
